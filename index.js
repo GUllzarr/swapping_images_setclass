e444b803279e7d68fa6fb7c6ed3633b6c8eabe03
GUllzarr <160250097+GUllzarr@users.noreply.github.com>
2024-09-18 10:06:48 -0700

    image swap and set classes

---
+++ index.js
@@ -0,0 +1,16 @@
+function swapImage(newImageSrc, clickedImage) {
+    var mainImage = document.getElementById("mainImage");
+    // swap images
+    mainImage.src = newImageSrc;
+  
+    // Remove 'active' class from all thumbnails
+    var images = document.querySelectorAll(".gallery img");
+    // getElementById vs querySelector vs querySelectorAll
+    // images.forEach(img => img.classList.remove("active"));
+    for (var i = 0; i < images.length; i++) {
+      images[i].classList.remove("active")
+    }
+  
+    // Add 'active' class to the clicked thumbnail
+    clickedImage.classList.add("active");
+  }
